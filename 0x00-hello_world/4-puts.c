5c4ac973906d000873614c84d6f895ea87531311
justme80sj <shuaibjamiu80@gmail.com>
2022-12-08 16:13:33 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
-
-
 int main(void)
-
 { 
-
-	puts("\"Programming is like building a multi lingual puzzle);
-
-	return (0) 
+	puts("\"Programming is like building a multi lingual puzzle");
+	return (0); 
 }
