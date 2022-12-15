410599daa49ed95382b8f3bc149704da4735a2ae
justme80sj <shuaibjamiu80@gmail.com>
2022-12-15 08:28:31 -0800

    lart largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,17 +16,17 @@ if (a > b && a > c)
 {
 largest = a;
 }
-else if (b > a && b > c)
-{
-largest = b;
-}
-else if (c > b)
+else if (a > b && c > a)
 {
 largest = c;
 }
-else
+else if (b > c)
 {
 largest = b;
+}
+else
+{
+largest = c;
 }
 return (largest);
 }
