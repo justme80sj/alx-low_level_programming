85786b9d765ff91c5925a7b04b2d9d944cf78db5
justme80sj <shuaibjamiu80@gmail.com>
2022-12-15 13:18:25 -0800

    supper man

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if j is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if ((j >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
