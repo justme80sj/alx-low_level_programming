12d6bb2c03124fb5c05f7ea24e051b8c4b0b2677
justme80sj <shuaibjamiu80@gmail.com>
2022-12-19 15:27:10 -0800

    important commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,11 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+int i;
 
-	for (i = 0; i < (n - 1); i++)
-		printf("%d, ", a[i]);
-		if (i == (n - 1))
-			printf("%d", a[n - 1]);
-	printf("\n");
+for (i = 0; i < (n - 1); i++)
+printf("%d, ", a[i]);
+if (i == (n - 1))
+printf("%d", a[n - 1]);
+printf("\n");
 }
