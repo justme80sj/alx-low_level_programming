d0cdf7abd0164ea98604e1b6404fc0eaddeb7fef
justme80sj <shuaibjamiu80@gmail.com>
2022-12-21 08:35:59 -0800

    far away commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: two concat strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int m, p;
+
+	for (m = 0; dest[m] != '\0'; m++)
+	{
+	}
+
+	for (p = 0; (dest[m + p] = *src++) != '\0'; p++)
+	{
+	}
+
+	return (dest);
+}
