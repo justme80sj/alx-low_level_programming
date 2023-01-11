32d520c0345fedb4e5b6d90fdd926e35917481b4
justme80sj <shuaibjamiu80@gmail.com>
2023-01-10 16:23:46 -0800

    clean sheet

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,20 @@
-#include "main.h"
+/**
+ * File: 5-sqrt_recursion.c
+ * Auth: shuaib
+ */
 
-int squareroot(int n, int i);
+#include "main.h"
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 
 /**
  * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
  * @n: input
  * Return: Always 0 (Success)
  */
+int find_sqrt(int num, int root);
 
-int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	else
-		return (squareroot(n, (n + 1) / 2));
-}
-/**
- * squareroot - checks if perfect square
- * @n: input
- * @i: counter
- * Return: if square root
- */
-
-int squareroot(int n, int i)
-{
-	if (i < 1)
-		return (-1);
-	else if (i * i == n)
-		return (i);
-	else
-		return (squareroot(n, i - 1));
+	if ((root * iroot) == num)
+		return (root);
 }
