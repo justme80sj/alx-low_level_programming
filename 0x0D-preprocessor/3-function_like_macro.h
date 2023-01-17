aa8446734c3c6689c2ba7f0047bd454553849f5c
justme80sj <shuaibjamiu80@gmail.com>
2023-01-16 18:22:10 -0800

    primary commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
+#ifndef ABSO
+#define ABSO
 
-#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
+#define ABS(x) (((x) < (0)) ? (-(x) : (x))
 
-#endif /* FUNCTION_LIKE_MACRO */
+#endif /* ABS(x) */
