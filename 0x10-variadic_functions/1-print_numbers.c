d6521301386ce05eef12b7fd160bf37ef471f494
justme80sj <shuaibjamiu80@gmail.com>
2023-01-19 14:07:47 -0800

    what a commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include "stdio.h"
+#include "stdarg.h"
+
+/**
+ * print_numbers - prints numbers followed by a newline
+ *
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ */
+
+void	print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list			args;
+	unsigned int	i = 0;
+
+	va_start(args, n);
+	while (i < n)
+	{
+		printf("%d", va_arg(args, int));
+		if (i != n - 1)
+			printf("%s", separator ? separator : "");
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+visit my world of technology
